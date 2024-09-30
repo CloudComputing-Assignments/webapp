af4b2a42fed2f2b5d23812263f6c29438f392ba4
aayush2378 <soni.aay@northeastern.edu>
2024-09-30 00:15:56 -0400

    Add user creation

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const { healthz } = require('./controllers/healthzController');
 const healthzRouter = require('./routes/healthzRouter');
+const userRouter = require('./routes/userRouter');
 const app = express();
 const port = 3000;
-const { rejectNonGetMiddleware, rejectPayloadMiddleware, handleSyntaxError } = require('./middleware/middleware');
+const { handleSyntaxError } = require('./middleware/middleware');
+const { sequelize } = require('./config/config'); // Adjust the path as necessary
 
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('Database & tables created!');
+  })
+  .catch((error) => {
+    console.error('Error creating database tables:', error);
+  });
 
 // Apply middleware
 app.use(express.json());
-app.use(rejectNonGetMiddleware);
-app.use(rejectPayloadMiddleware);
 
 // Define routes
 app.get("/", (req, res) => {
@@ -18,6 +25,7 @@ app.get("/", (req, res) => {
 });
 
 app.use('/healthz', healthzRouter);
+app.use('/v1/user', userRouter);
 
 // Error handler for malformed JSON (or any syntax error)
 app.use(handleSyntaxError);
