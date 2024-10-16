a274efb4cd3391ff51e025b88442320fd6b8fc9c
aayush2378 <soni.aay@northeastern.edu>
2024-10-16 14:04:54 -0400

    Edit packer script

--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -34,26 +34,26 @@ else
     exit 1
 fi
 
-sudo mysql --execute="CREATE USER '${DB_USERNAME}'@'localhost' IDENTIFIED WITH mysql_native_password BY '${DB_PASSWORD}';"
+sudo mysql --user="${DB_USERNAME}" --password="${DB_PASSWORD}" --execute="ALTER USER '${DB_USERNAME}'@'localhost' IDENTIFIED WITH mysql_native_password BY '${DB_PASSWORD}';"
 if [ $? -eq 0 ]; then
-    echo "User ${DB_USERNAME} created successfully."
+    echo "User ${DB_USERNAME} altered successfully."
 else
     echo "Failed to create user ${DB_USERNAME}."
     exit 1
 fi
 
-sudo mysql --execute="GRANT ALL PRIVILEGES ON ${DB_DATABASE}.* TO '${DB_USERNAME}'@'localhost';"
-if [ $? -eq 0 ]; then
-    echo "Privileges granted to user ${DB_USERNAME} on database ${DB_DATABASE}."
-else
-    echo "Failed to grant privileges to user ${DB_USERNAME}."
-    exit 1
-fi
+# sudo mysql --execute="GRANT ALL PRIVILEGES ON ${DB_DATABASE}.* TO '${DB_USERNAME}'@'localhost';"
+# if [ $? -eq 0 ]; then
+#     echo "Privileges granted to user ${DB_USERNAME} on database ${DB_DATABASE}."
+# else
+#     echo "Failed to grant privileges to user ${DB_USERNAME}."
+#     exit 1
+# fi
 
-sudo mysql --execute="FLUSH PRIVILEGES;"
-if [ $? -eq 0 ]; then
-    echo "Privileges flushed successfully."
-else
-    echo "Failed to flush privileges."
-    exit 1
-fi
+# sudo mysql --execute="FLUSH PRIVILEGES;"
+# if [ $? -eq 0 ]; then
+#     echo "Privileges flushed successfully."
+# else
+#     echo "Failed to flush privileges."
+#     exit 1
+# fi
